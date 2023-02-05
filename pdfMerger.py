2e8dd57ba4512545c8078e6b02fd1bf623718664
Rami5500 <Ramih5500@outlook.com>
2023-02-05 20:26:19 +0000

    First commit

---
+++ pdfMerger.py
@@ -0,0 +1,16 @@
+import PyPDF2
+import sys
+#os module is used to work with directorys(folders), e.g; fetching its contents
+import os
+
+#creates a merger object
+merger = PyPDF2.PdfFileMerger()
+
+for file in os.listdir(os.curdir):
+    if file.endswith(".pdf"):
+        #To test it prints out the correct file
+        #print(file)
+
+        merger.append(file)
+
+    merger.write("combined_Files.pdf")
